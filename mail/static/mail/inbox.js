e431faca4fe17fcd5eace2a324122eeaea3d1fd8
Aoibheann <aoibheannfegan04@gmail.com>
2024-03-21 11:12:11 -0700

    Updating CSS

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -3,16 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#archive').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#compose-form').addEventListener('submit', send_email);
+  document.querySelectorAll('.nav-button').forEach(button => {
+    button.addEventListener('click', navButtonClicked)
+  })
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
+function navButtonClicked() {
+  document.querySelectorAll('.nav-button').forEach(button => {
+    button.classList.remove('selected')
+  })
+  this.classList.add('selected')
+}
 
+function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -25,8 +34,6 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
-  // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#individual-email').style.display = 'none';
@@ -38,19 +45,15 @@ function load_mailbox(mailbox) {
   fetch('/emails/' + mailbox)
   .then(response => response.json())
   .then(emails => {
-      // Print emails
-      console.log(emails);
-
       //add email details to div
       emails.forEach(email => {
         const emailElement = document.createElement('div');
-        emailElement.className = 'email-preview';
         emailElement.className = email.read ? 'read' : 'unread';
         emailElement.innerHTML = `
-          <h8 class="email-sender">${email.sender}</h8>
           <h8 class="email-subject">${email.subject}</h8>
+          <h8 class="email-sender">${email.sender}</h8>
           <h8 class="email-timestamp">${email.timestamp}</h8>
-          `
+        `
         ;
         //if an email in the view is clicked then load a view of email
         emailElement.addEventListener('click', () => email_view(email.id, mailbox));
@@ -71,34 +74,36 @@ function email_view(email_id, mailbox) {
   fetch('/emails/' + email_id)
   .then(response => response.json())
   .then(email => {
-      // Print email
-      console.log(email);
-
       //generate content to add to individual email div 
       const element = document.createElement('div');
+      element.classList.add('email-body')
       element.innerHTML = `
-        <h2>${email.subject}</h2>
-        <div>From: ${email.sender}</div>
-        <div>To: ${email.recipients}</div>
-        <div>${email.timestamp}</div>
-        <p class = "email-body">${email.body}</p>
+        <h2 class="email-body-content">${email.subject}</h2>
+        <div class="email-body-content">From: ${email.sender}</div>
+        <div class="email-body-content">To: ${email.recipients}</div>
+        <div class="email-body-content date">${email.timestamp}</div>
+        <p class="email-body-content">${email.body}</p>
       `
       ;
+
+      let buttonContainer = document.createElement('div')
+      buttonContainer.id="button-container"
+      element.appendChild(buttonContainer)
       //render an archive button if email has not been sent from current user
       if (mailbox !== 'sent') {
         const archiveButton = document.createElement('button')
         archiveButton.innerText = email.archived ? 'Unarchive' : 'Archive';
         archiveButton.addEventListener('click', () => archive_email(email.id, !email.archived));
-        archiveButton.classList.add('btn', 'btn-outline-danger');
-        element.appendChild(archiveButton);
+        archiveButton.classList.add('archive-button');
+        buttonContainer.appendChild(archiveButton);
       }
 
       //create a reply button
       const replyButton = document.createElement('button')
       replyButton.innerText = 'Reply';
       replyButton.addEventListener('click', () => reply(email.id));
-      replyButton.classList.add('btn', 'btn-outline-success');
-      element.appendChild(replyButton);
+      replyButton.classList.add('reply-button');
+      buttonContainer.appendChild(replyButton);
 
       //add remaining email details to view and ensure only view is visible
       document.querySelector('#individual-email').append(element);
@@ -153,6 +158,8 @@ function reply(email_id) {
 
 function send_email(event) {
   event.preventDefault();
+  document.querySelector('#sent').classList.add('selected');
+  document.querySelector('#compose').classList.remove('selected');
   let recipients = document.querySelector('#compose-recipients').value
   let subject = document.querySelector('#compose-subject').value
   let body = document.querySelector('#compose-body').value
